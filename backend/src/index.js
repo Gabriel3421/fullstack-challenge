78aa2916693d94b3d7ab2445f8db8d0aacca3505
gabriel3421 <gabrielsouza.n.s@hotmail.com>
2021-11-20 11:06:00 -0300

    add cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,11 @@
 const express = require("express")
+const cors = require('cors');
 require('./database');
 const orderRoutes = require("./routes/orderRoutes")
 const categoryRoutes = require("./routes/categoryRoutes")
 const app = express()
 app.use(express.json())
-
+app.use(cors());
 app.use("/orders", orderRoutes)
 app.use("/categories", categoryRoutes)
 
